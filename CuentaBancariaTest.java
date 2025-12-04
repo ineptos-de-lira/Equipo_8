38f46a8633d36cf296d24d45192c2f16659b9dee
m0loc89 <up240716@alumnos.upa.edu.mx>
2025-12-03 20:40:32 -0600

    Test adicionales

--- CuentaBancariaTest.java
+++ CuentaBancariaTest.java
@@ -2,24 +2,24 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test; 
 
 /**
- * Pruebas unitarias de la clase CuentaBancaria.
+ * Pruebas unitarias.
  */
 public class CuentaBancariaTest {
 
   @Test
-  public void pruebaAñadirDineroValido() {
+  public void pruebaagregarDineroValido() {
     CuentaBancaria cuenta = new CuentaBancaria(0);
-    assertTrue(cuenta.añadirDinero(100));
+    assertTrue(cuenta.agregarDinero(100));
     assertEquals(100, cuenta.getSaldo());
   }
   
   @Test
-  public void pruebaAñadirDineroInvalido() {
+  public void pruebaagregarDineroInvalido() {
     CuentaBancaria cuenta = new CuentaBancaria(0);
-    assertFalse(cuenta.añadirDinero(-50));
+    assertFalse(cuenta.agregarDinero(-50));
     assertEquals(0, cuenta.getSaldo());
   }
 
@@ -37,5 +37,25 @@ public class CuentaBancariaTest {
     assertEquals(50, cuenta.getSaldo());
   }
 
+  @Test
+  public void pruebaDonacionValida() {
+    CuentaBancaria cuenta = new CuentaBancaria(300);
+    assertTrue(cuenta.pagarServicio(100));
+    assertEquals(200, cuenta.getSaldo());
+  }
+
+  @Test
+  public void pruebaPagarServicioValido() {
+    CuentaBancaria cuenta = new CuentaBancaria(600);
+    assertTrue(cuenta.pagarServicio(250));
+    assertEquals(350, cuenta.getSaldo());
+  }
+
+  @Test
+  public void pruebaPagarServicioInsuficiente() {
+    CuentaBancaria cuenta = new CuentaBancaria(50);
+    assertFalse(cuenta.pagarServicio(200));
+    assertEquals(50, cuenta.getSaldo());
+  }
 
 }
